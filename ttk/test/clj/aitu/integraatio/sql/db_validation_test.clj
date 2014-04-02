97c936fb3460deccd7bb72be906df2053e6fef33
Antti Virtanen <antti.virtanen@iki.fi>
2014-04-02 16:59:49 +0300

    OPH-398 validointikyselyille oma integraatiotesti joka varmistaa ainakin sen että SQL kyselyn syntaksi on ok

---
+++ ttk/test/clj/aitu/integraatio/sql/db_validation_test.clj
@@ -0,0 +1,15 @@
+(ns aitu.integraatio.sql.db-validation-test
+  (:require
+    [aitu.integraatio.sql.validationtest :as validationtest]
+    [clojure.test :refer [deftest testing is]]
+    [korma.core :as sql]
+    [aitu.integraatio.sql.test-util :refer :all]))
+
+(defn failed? [kysely]
+  (not (empty? (:results kysely))))
+
+(deftest ^:integraatio tarkista-tietokanta-sql-validointi
+  (testing "tarkistetaan että tietokannan SQL validoinnit menevät läpi. Ilman konversiodataa lähinnä syntaktinen tarkastus."
+    (tietokanta-fixture
+      #(let [tulokset (validationtest/run-queries! @validationtest/default-query-list)]
+         (is (empty? (filter failed? tulokset)))))))
