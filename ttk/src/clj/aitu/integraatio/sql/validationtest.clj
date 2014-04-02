ee7f13fb55c018c8d0ad9aa6d501a4cd8290aeb0
Antti Virtanen <antti.virtanen@iki.fi>
2014-04-02 17:07:34 +0300

    OPH-398

--- ttk/src/clj/aitu/integraatio/sql/validationtest.clj
+++ ttk/src/clj/aitu/integraatio/sql/validationtest.clj
@@ -6,15 +6,18 @@
             [clojure.tools.logging :as log]
             [korma.db :as db]))
 
-(defn run-queries-from-file! [filepath]
-  (with-open [istream (io/input-stream filepath)] ; TODO: tiedoston cachetus. Ei avata tiedostoa joka requestille
-    (let [query-list (solita.util.validationtest.SmokeTestSqlReader/readAll istream)
-          _ (log/info "Ajetaan tietokannan validointitestit: " (count query-list) " kappaletta")
+(defn load-validation-queries! [filepath] 
+  (with-open [istream (io/input-stream filepath)] 
+    (solita.util.validationtest.SmokeTestSqlReader/readAll istream)))
+
+(def default-query-list 
+  (delay (load-validation-queries! "resources/validationtests.sql")))
+  
+(defn run-queries! [query-list]
+    (let [_ (log/info "Ajetaan tietokannan validointitestit: " (count query-list) " kappaletta")
           con (.getConnection (:datasource (:pool @db/_default)))]
-      
       (into [] (doall (for [query query-list]
                         (let [tulos {:title (.getQueryTitle query)
                                      :results (jdbc/query {:connection con} [(.getQuerySql query)])}]
-                        (log/info "Kysely " (:title tulos) " ongelmia " (count (:results tulos)) " kpl")
-                        tulos)))))))
-
+                          (log/info "Kysely " (:title tulos) " ongelmia " (count (:results tulos)) " kpl")
+                          tulos))))))
