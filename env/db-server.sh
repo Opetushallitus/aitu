0776d694b5f7907ed8f5f9503c83ca02c6c7d4e8
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2014-04-17 09:28:35 +0300

    OPH-563 tietokantapalvelimen luontiskripti odottamaan Postgresql:n käynnistymistä

--- env/db-server.sh
+++ env/db-server.sh
@@ -17,4 +17,9 @@ iptables -I INPUT 1 -p tcp -s $APP_HOST --dport 5432 -j ACCEPT
 service iptables save
 
 # alustetaan ttk tietokanta ilman tauluja
+
+until service postgresql-9.2 status > /dev/null; do
+  echo "Waiting for postgresql...\n"
+  sleep 1
+done
 su postgres -c 'psql --file=db/dev.sql'
