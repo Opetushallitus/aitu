bee5a9cccdd8649be3fa15e18650e64063adc3c4
Timo Mihaljov <timo.mihaljov@solita.fi>
2014-05-12 13:48:16 +0300

    OPH-492: Lisätään dev-avain ssh-addilla ennen Vagrantiin julkaisua

--- dev-scripts/deploy.sh
+++ dev-scripts/deploy.sh
@@ -18,4 +18,6 @@ cd $repo_path/ttk-db
 lein do clean, uberjar
 
 cd $repo_path/ansible
+chmod 600 yhteiset/dev_id_rsa
+ssh-add yhteiset/dev_id_rsa
 ansible-playbook -v -i aitu_vagrant/hosts yhteiset/julkaise_paikallinen_versio.yml -e "sovellus_jar=\"$repo_path/ttk/target/ttk-standalone.jar\" migraatio_jar=\"$repo_path/ttk-db/target/ttk-db-standalone.jar\""
