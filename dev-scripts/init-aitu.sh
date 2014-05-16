b27234bfce668d79f8280f18d8f0bd2c1d6bbb94
Timo Mihaljov <timo.mihaljov@solita.fi>
2014-05-16 10:46:37 +0300

    init-aitu.sh: Alustetaan myös tietokantapalvelin

--- dev-scripts/init-aitu.sh
+++ dev-scripts/init-aitu.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 set -eu
 
-REPO_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+repo_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 
 set -x
 
-read -p "Alustetaanko CAS [K/e]? " cas
+read -p "Alustetaanko CAS-palvelin [K/e]? " cas
+read -p "Alustetaanko tietokantapalvelin [K/e]? " db
 
-cd $REPO_PATH/vagrant
+cd $repo_path/vagrant
 
-if [[ $cas == [Kk]* ]] || [[ $cas == "" ]] ;
-then
+if [[ $cas == [Kk]* ]] || [[ $cas == "" ]]; then
   vagrant destroy -f auth
   vagrant up auth
+fi
+
+if [[ $db == [Kk]* ]] || [[ $db == "" ]]; then
+  $repo_path/dev-scripts/init-db.sh
 fi
 
 vagrant destroy -f aitu
 vagrant up aitu
 
-$REPO_PATH/dev-scripts/deploy.sh
+$repo_path/dev-scripts/deploy.sh
