8687d566575a2152d1eee117b879f9bd5a0bcdcb
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2014-05-30 12:34:59 +0300

    OPH-641 käyttöoikeuspalvelun integraatioon toimikuntajäsenen rooli

--- ttk/src/clj/aitu/integraatio/kayttooikeuspalvelu.clj
+++ ttk/src/clj/aitu/integraatio/kayttooikeuspalvelu.clj
@@ -19,45 +19,46 @@
             [aitu.toimiala.kayttajaoikeudet :refer [kayttajaroolit]]))
 
 (def aitu-organisaatio "1.2.246.562.10.2013121312395140176502")
+(def toimikunnat-organisaatio "1.2.246.562.10.59113820717")
 
-(defn aitu-ryhma-cn [nimi]
-  (str "APP_AITU_" nimi "_" aitu-organisaatio))
+(defn aitu-ryhma-cn [organisaatio nimi]
+  (str "APP_AITU_" nimi "_" organisaatio))
 
-(def roolin-ryhma-cn
-  {(:yllapitaja kayttajaroolit) (aitu-ryhma-cn "CRUD")
-   (:kayttaja kayttajaroolit) (aitu-ryhma-cn "READ_UPDATE")
-   (:osoitepalvelu kayttajaroolit) (aitu-ryhma-cn "OSOITEPALVELU")})
+(def roolin-ryhma-cnt
+  {(:yllapitaja kayttajaroolit) [(aitu-ryhma-cn aitu-organisaatio "CRUD")]
+   (:kayttaja kayttajaroolit) [(aitu-ryhma-cn toimikunnat-organisaatio "READ_UPDATE")
+                               (aitu-ryhma-cn aitu-organisaatio "READ_UPDATE")]
+   (:osoitepalvelu kayttajaroolit) [(aitu-ryhma-cn aitu-organisaatio "OSOITEPALVELU")]})
+
 
 (defn ryhma-dn [ryhma-cn]
   (str "cn=" ryhma-cn ",ou=Groups,dc=opintopolku,dc=fi"))
 
 (defn kayttajat [kayttooikeuspalvelu rooli]
-  {:pre [(contains? roolin-ryhma-cn rooli)]}
-  (let [yhteys (kayttooikeuspalvelu)
-        roolin-ryhma-dn (ryhma-dn (roolin-ryhma-cn rooli))]
-    (try
-      (if-let [ryhma (ldap/get yhteys roolin-ryhma-dn)]
-        (let [kayttaja-dnt (:uniqueMember ryhma)
-              ;; Jos ryhmällä on vain yksi uniqueMember-attribuutti, clj-ldap
-              ;; palauttaa arvon (stringin) eikä vektoria arvoista.
-              kayttaja-dnt (if (string? kayttaja-dnt)
-                             [kayttaja-dnt]
-                             kayttaja-dnt)]
-          (doall
-            (for [kayttaja-dn kayttaja-dnt
-                  :let [kayttaja (ldap/get yhteys kayttaja-dn)
-                        _ (assert kayttaja)
-                        [etunimi toinennimi] (s/split (:cn kayttaja) #" ")
-                        sukunimi (:sn kayttaja)]]
-              {:oid (:employeeNumber kayttaja)
-               :uid (:uid kayttaja)
-               :etunimi etunimi
-               :sukunimi (or sukunimi "")
-               :rooli rooli})))
-        (log/warn "Roolin" rooli "ryhmää" roolin-ryhma-dn
-                  "ei löytynyt, ei lueta roolin käyttäjiä"))
-      (finally
-        (.close yhteys)))))
+  {:pre [(contains? roolin-ryhma-cnt rooli)]}
+  (with-open [yhteys (kayttooikeuspalvelu)]
+    (apply concat (for [cn (roolin-ryhma-cnt rooli)
+                        :let [roolin-ryhma-dn (ryhma-dn cn)]]
+                    (if-let [ryhma (ldap/get yhteys roolin-ryhma-dn)]
+                      (let [kayttaja-dnt (:uniqueMember ryhma)
+                            ;; Jos ryhmällä on vain yksi uniqueMember-attribuutti, clj-ldap
+                            ;; palauttaa arvon (stringin) eikä vektoria arvoista.
+                            kayttaja-dnt (if (string? kayttaja-dnt)
+                                           [kayttaja-dnt]
+                                           kayttaja-dnt)]
+                        (doall
+                          (for [kayttaja-dn kayttaja-dnt
+                                :let [kayttaja (ldap/get yhteys kayttaja-dn)
+                                      _ (assert kayttaja)
+                                      [etunimi toinennimi] (s/split (:cn kayttaja) #" ")
+                                      sukunimi (:sn kayttaja)]]
+                            {:oid (:employeeNumber kayttaja)
+                             :uid (:uid kayttaja)
+                             :etunimi etunimi
+                             :sukunimi (or sukunimi "")
+                             :rooli rooli})))
+                         (log/warn "Roolin" rooli "ryhmää" roolin-ryhma-dn
+                                   "ei löytynyt, ei lueta roolin käyttäjiä"))))))
 
 (defn tee-kayttooikeuspalvelu [ldap-auth-server-asetukset]
   (fn []
