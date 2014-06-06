c772170c37a275a4821313dda42d3752a40b5776
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2014-06-06 13:31:35 +0300

    Koodistopalvelun eräajoon tutkinnon voimassaolon tarkastelu

--- ttk/src/clj/aitu/infra/eraajo/tutkinnot.clj
+++ ttk/src/clj/aitu/infra/eraajo/tutkinnot.clj
@@ -30,11 +30,11 @@
     (merge {:tutkintotunnus tutkintotunnus}
           (uudet-arvot muutos))))
 
-(defn tallenna-uudet-opintoalat [opintoalat]
+(defn tallenna-uudet-opintoalat! [opintoalat]
   (doseq [ala opintoalat]
     (opintoala-arkisto/lisaa! ala)))
 
-(defn tallenna-muuttuneet-opintoalat [opintoalat]
+(defn tallenna-muuttuneet-opintoalat! [opintoalat]
   (doseq [ala opintoalat]
     (opintoala-arkisto/paivita! ala)))
 
@@ -42,104 +42,119 @@
   (clojure.set/rename-keys ala {:nimi_fi :selite_fi
                                 :nimi_sv :selite_sv}))
 
-(defn tallenna-opintoalat [opintoalat]
+(defn tallenna-opintoalat! [opintoalat]
   (let [uudet (for [[alakoodi ala] opintoalat
-                    :when (vector? ala)]
+                    :when (and (vector? ala) (first ala))]
                 (nimi->selite (first ala)))
         muuttuneet (for [[alakoodi ala] opintoalat
                          :when (map? ala)]
                      (nimi->selite (uudet-arvot ala)))]
-    (tallenna-uudet-opintoalat uudet)
-    (tallenna-muuttuneet-opintoalat muuttuneet)))
+    (tallenna-uudet-opintoalat! uudet)
+    (tallenna-muuttuneet-opintoalat! muuttuneet)))
 
-(defn tallenna-uudet-koulutusalat [koulutusalat]
+(defn tallenna-uudet-koulutusalat! [koulutusalat]
   (doseq [ala koulutusalat]
     (koulutusala-arkisto/lisaa! ala)))
 
-(defn tallenna-muuttuneet-koulutusalat [koulutusalat]
+(defn tallenna-muuttuneet-koulutusalat! [koulutusalat]
   (doseq [ala koulutusalat]
     (koulutusala-arkisto/paivita! ala)))
 
-(defn nimi->selite [ala]
-  (clojure.set/rename-keys ala {:nimi_fi :selite_fi
-                                :nimi_sv :selite_sv}))
-
-(defn tallenna-koulutusalat [koulutusalat]
+(defn tallenna-koulutusalat! [koulutusalat]
+  (>pprint koulutusalat)
   (let [uudet (for [[alakoodi ala] koulutusalat
                     :when (vector? ala)]
                 (nimi->selite (first ala)))
         muuttuneet (for [[alakoodi ala] koulutusalat
                          :when (map? ala)]
                      (nimi->selite (uudet-arvot ala)))]
-    (tallenna-uudet-koulutusalat uudet)
-    (tallenna-muuttuneet-koulutusalat muuttuneet)))
+    (tallenna-uudet-koulutusalat! uudet)
+    (tallenna-muuttuneet-koulutusalat! muuttuneet)))
 
-(defn tallenna-uudet-tutkinnonosat [tutkintoversio-id tutkinnonosat]
+(defn tallenna-uudet-tutkinnonosat! [tutkintoversio-id tutkinnonosat]
   (doseq [{:keys [jarjestysnumero tutkinnonosa]} tutkinnonosat]
     (tutkinto-arkisto/lisaa-tutkinnon-osa! tutkintoversio-id
-                                          jarjestysnumero
-                                          (assoc tutkinnonosa :versio 1))))
+                                           jarjestysnumero
+                                           (assoc tutkinnonosa :versio 1))))
 
-(defn tallenna-muuttuneet-tutkinnonosat [tutkintoversio-id tutkinnonosat]
+(defn tallenna-muuttuneet-tutkinnonosat! [tutkintoversio-id tutkinnonosat]
   (doseq [{:keys [jarjestysnumero tutkinnonosa]} tutkinnonosat]
     (tutkinto-arkisto/paivita-tutkinnon-osa! tutkintoversio-id
                                              jarjestysnumero
                                              tutkinnonosa)))
 
-(defn tallenna-tutkinnonosat [tutkintoversio-id tutkinnonosat]
+(defn tallenna-tutkinnonosat! [tutkintoversio-id tutkinnonosat]
   (let [uudet (for [osa tutkinnonosat
                     :when (vector? (:tutkinnonosa osa))]
                 (update-in osa [:tutkinnonosa] first))
         muuttuneet (for [osa tutkinnonosat
                          :when (map? (:tutkinnonosa osa))]
                      (update-in osa [:tutkinnonosa] uudet-arvot))]
-    (tallenna-uudet-tutkinnonosat tutkintoversio-id uudet)
-    (tallenna-muuttuneet-tutkinnonosat tutkintoversio-id muuttuneet)))
+    (tallenna-uudet-tutkinnonosat! tutkintoversio-id uudet)
+    (tallenna-muuttuneet-tutkinnonosat! tutkintoversio-id muuttuneet)))
 
-(defn tallenna-uudet-osaamisalat [tutkintoversio-id osaamisalat]
+(defn tallenna-uudet-osaamisalat! [tutkintoversio-id osaamisalat]
   (doseq [osaamisala osaamisalat]
     (tutkinto-arkisto/lisaa-osaamisala! (assoc osaamisala
                                                :versio 1
                                                :tutkintoversio tutkintoversio-id))))
 
-(defn tallenna-muuttuneet-osaamisalat [tutkintoversio-id osaamisalat]
+(defn tallenna-muuttuneet-osaamisalat! [tutkintoversio-id osaamisalat]
   (doseq [osaamisala osaamisalat]
     (tutkinto-arkisto/paivita-osaamisala! (assoc osaamisala :tutkintoversio tutkintoversio-id))))
 
 (defn tallenna-osaamisalat! [tutkintoversio-id osaamisalat]
   (let [uudet (keep uusi osaamisalat)
         muuttuneet (keep muuttunut osaamisalat)]
-    (tallenna-uudet-osaamisalat tutkintoversio-id uudet)
-    (tallenna-muuttuneet-osaamisalat tutkintoversio-id muuttuneet)))
+    (tallenna-uudet-osaamisalat! tutkintoversio-id uudet)
+    (tallenna-muuttuneet-osaamisalat! tutkintoversio-id muuttuneet)))
 
-(defn paivita-tutkinnot! [tutkintomuutokset]
+(defn paivita-tutkinto! [koodistoasetukset tutkinto]
+  (when (:jarjestyskoodistoversio tutkinto)
+    (let [vanha-tutkintoversio (tutkinto-arkisto/hae-tutkinto (:tutkintotunnus tutkinto))
+          jarjestyskoodisto (koodisto/hae-koodisto koodistoasetukset (:osajarjestyskoodisto vanha-tutkintoversio) (:jarjestyskoodistoversio vanha-tutkintoversio))]
+      (tutkinto-arkisto/paivita-tutkintoversio! {:tutkintoversio_id (:tutkintoversio_id vanha-tutkintoversio)
+                                                 :voimassa_loppupvm (:voimassa_loppupvm jarjestyskoodisto)})))
+  (if (every? nil? ((juxt :osaamisalat :tutkinnonosat) tutkinto))
+    (let [tutkintotunnus (:tutkintotunnus tutkinto)
+          tutkintotiedot (select-keys tutkinto [:nimi_fi :nimi_sv :tyyppi :tutkintotaso :opintoala])
+          versiotiedot (select-keys tutkinto [:voimassa_alkupvm :voimassa_loppupvm])]
+      (when (not-every? nil? (vals tutkintotiedot))
+        (tutkinto-arkisto/paivita! tutkintotunnus tutkintotiedot))
+      (when (not-every? nil? (vals versiotiedot))
+        (let [tutkintoversio-id (:tutkintoversio_id (tutkinto-arkisto/hae-tutkinto (:tutkintotunnus tutkinto)))]
+          (tutkinto-arkisto/paivita-tutkintoversio! (assoc versiotiedot :tutkintoversio_id tutkintoversio-id)))))
+    (tutkinto-arkisto/paivita-tutkinto! (dissoc tutkinto :osaamisalat :tutkinnonosat))))
+
+(defn paivita-tutkinnot! [koodistoasetukset tutkintomuutokset]
   (try
     (db/transaction
       (let [opintoalat (set (map :opintoala_tkkoodi (opintoala-arkisto/hae-kaikki)))
             {:keys [tutkinnot osaamisalat tutkinnonosat]} tutkintomuutokset]
         (doseq [t (keep uusi tutkinnot)
                 :when (contains? opintoalat (:opintoala t))]
-          (let [tutkintoversio-id (tutkinto-arkisto/lisaa-tutkinto-ja-versio! (dissoc t :osaamisalat :tutkinnonosat))
+          (let [tutkintoversio-id (tutkinto-arkisto/lisaa-tutkinto-ja-versio! (assoc (dissoc t :osaamisalat :tutkinnonosat)
+                                                                                     :versio 1))
                 osaamisalat (map osaamisalat (:osaamisalat t))
                 tutkinnonosat (for [{:keys [osatunnus jarjestysnumero]} (:tutkinnonosat t)]
                                 {:jarjestysnumero jarjestysnumero
                                  :tutkinnonosa (tutkinnonosat osatunnus)})]
-            (tallenna-osaamisalat tutkintoversio-id osaamisalat)
-            (tallenna-tutkinnonosat tutkintoversio-id tutkinnonosat)))
+            (tallenna-osaamisalat! tutkintoversio-id osaamisalat)
+            (tallenna-tutkinnonosat! tutkintoversio-id tutkinnonosat)))
         (doseq [t (keep muuttunut tutkinnot)
                 :when (contains? opintoalat (:opintoala t))]
-          (let [tutkintoversio-id (tutkinto-arkisto/paivita-tutkinto! (dissoc t :osaamisalat :tutkinnonosat))
+          (let [tutkintoversio-id (paivita-tutkinto! koodistoasetukset t)
                 osaamisalat (map osaamisalat (:osaamisalat t))
                 tutkinnonosat (for [{:keys [osatunnus jarjestysnumero]} (:tutkinnonosat t)]
                                 {:jarjestysnumero jarjestysnumero
                                  :tutkinnonosa (tutkinnonosat osatunnus)})]
-            (tallenna-osaamisalat tutkintoversio-id osaamisalat)
-            (tallenna-tutkinnonosat tutkintoversio-id tutkinnonosat)))))
+            (tallenna-osaamisalat! tutkintoversio-id osaamisalat)
+            (tallenna-tutkinnonosat! tutkintoversio-id tutkinnonosat)))))
     (catch org.postgresql.util.PSQLException e
       (log/error e "Tutkintojen päivitys koodistopalvelusta epäonnistui"))))
 
 (defn paivita-tutkinnot-koodistopalvelusta! [asetukset]
   (tallenna-koulutusalat! (koodisto/koulutusala-muutokset asetukset))
   (tallenna-opintoalat! (koodisto/opintoala-muutokset asetukset))
-  (paivita-tutkinnot! (koodisto/tutkinto-muutokset asetukset)))
+  (paivita-tutkinnot! asetukset (koodisto/tutkinto-muutokset asetukset)))
 
