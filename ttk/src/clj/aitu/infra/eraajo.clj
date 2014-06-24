3aa6d75851d96f5052a9f17f9b3856d54fcd93f9
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2014-06-24 10:31:27 +0300

    Päivittäinen eräajo organisaatiopalveluintegraatiolle

--- ttk/src/clj/aitu/infra/eraajo.clj
+++ ttk/src/clj/aitu/infra/eraajo.clj
@@ -19,24 +19,36 @@
             [clojurewerkz.quartzite.triggers :as t]
             [clojurewerkz.quartzite.schedule.daily-interval :as s]
             [clojure.tools.logging :as log]
-            aitu.infra.eraajo.kayttajat)
-  (:import aitu.infra.eraajo.kayttajat.PaivitaKayttajatLdapistaJob))
+            aitu.infra.eraajo.kayttajat
+            aitu.infra.eraajo.organisaatiot)
+  (:import aitu.infra.eraajo.kayttajat.PaivitaKayttajatLdapistaJob
+           aitu.infra.eraajo.organisaatiot.PaivitaOrganisaatiotJob))
 
 
-(defn kaynnista-ajastimet! [kayttooikeuspalvelu]
+(defn kaynnista-ajastimet! [kayttooikeuspalvelu organisaatiopalvelu-asetukset]
   (log/info "Käynnistetään ajastetut eräajot")
   (qs/initialize)
   (log/info "Poistetaan vanhat jobit ennen uudelleenkäynnistystä")
   (qs/clear!)
   (qs/start)
   (log/info "Eräajomoottori käynnistetty")
-  (let [job (j/build
-              (j/of-type PaivitaKayttajatLdapistaJob)
-              (j/with-identity "paivita-kayttajat-ldapista")
-              (j/using-job-data {"kayttooikeuspalvelu" kayttooikeuspalvelu}))
-        trigger (t/build
-                  (t/with-identity "5-min-valein")
-                  (t/start-now)
-                  (t/with-schedule (s/schedule
-                                     (s/with-interval-in-minutes 5))))]
-    (qs/schedule job trigger)))
+  (let [ldap-job (j/build
+                   (j/of-type PaivitaKayttajatLdapistaJob)
+                   (j/with-identity "paivita-kayttajat-ldapista")
+                   (j/using-job-data {"kayttooikeuspalvelu" kayttooikeuspalvelu}))
+        trigger-5min (t/build
+                       (t/with-identity "5-min-valein")
+                       (t/start-now)
+                       (t/with-schedule (s/schedule
+                                          (s/with-interval-in-minutes 5))))
+        org-job (j/build
+                  (j/of-type PaivitaOrganisaatiotJob)
+                  (j/with-identity "paivita-organisaatiot")
+                  (j/using-job-data {"asetukset" organisaatiopalvelu-asetukset}))
+        trigger-daily (t/build
+                        (t/with-identity "daily")
+                        (t/start-now)
+                        (t/with-schedule (s/schedule
+                                           (s/time-of-day 4 0 0))))]
+    (qs/schedule ldap-job trigger-5min)
+    (qs/schedule org-job trigger-daily)))
