a70552c02d1e47906a54deee8c9a7a17cddfa8b6
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2014-06-27 13:38:14 +0300

    Kasvatetaan e2e-commonin versionumeroa

--- aitu-common-e2e/project.clj
+++ aitu-common-e2e/project.clj
@@ -12,7 +12,7 @@
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; European Union Public Licence for more details.
 
-(defproject solita/opetushallitus-aitu-e2e "1.9.0"
+(defproject solita/opetushallitus-aitu-e2e "1.10.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-webdriver "0.6.1" :exclusions [org.seleniumhq.selenium/selenium-java
                                                      org.seleniumhq.selenium/selenium-server
