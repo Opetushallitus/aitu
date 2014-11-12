7a5183ff7583f32b88054f2ab6a2a829cd55addd
Timo Mihaljov <timo.mihaljov@solita.fi>
2014-11-12 13:30:14 +0200

    OPH-1029: Päivitetään tutkinnon nimi

---
+++ ttk-db/resources/db/migration/V15_0__keruutuotetarkastaja.sql
@@ -0,0 +1,5 @@
+-- https://issues.solita.fi/browse/OPH-1029
+update nayttotutkinto
+set nimi_fi='Luonnontuotealan erikoisammattitutkinto',
+    nimi_sv='Specialyrkesexamen inom branschen för naturprodukter'
+where tutkintotunnus='367901';
