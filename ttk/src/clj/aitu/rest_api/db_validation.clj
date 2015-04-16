3446796c8f2aff3d12d54bcdcf140861afe53b5d
Antti Virtanen <antti.virtanen@iki.fi>
2015-04-16 15:23:20 +0300

    Validointikyselyn tulokset selkeämmin näkyviin. JSON -> mustache-templaten avulla tehty HTML.

--- ttk/src/clj/aitu/rest_api/db_validation.clj
+++ ttk/src/clj/aitu/rest_api/db_validation.clj
@@ -2,10 +2,14 @@
   (:require [compojure.core :as c]
             [aitu.integraatio.sql.validationtest :as validationtest]
             [oph.common.util.http-util :refer [json-response]]
+            [stencil.core :as s]
             [aitu.compojure-util :as cu]))
 
 (def query-list @validationtest/default-query-list)
 
 (c/defroutes reitit
   (cu/defapi :status nil :get "/" []
-    (json-response (validationtest/run-queries! query-list))))
+    (let [results-raw (validationtest/run-queries! query-list)
+          results (map (fn [rm] {:title (:title rm) :results (map (fn [n] {:result n}) (:results rm))}) results-raw)]
+      (s/render-file "html/dbvalidation"
+                     {:testit results}))))
