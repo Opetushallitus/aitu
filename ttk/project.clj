55f6ed044fe4ef3a82cb96a15410bf1757e1c589
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2015-04-21 15:50:59 +0300

    Yhteiset java-koodit clojure-utilsiin

--- ttk/project.clj
+++ ttk/project.clj
@@ -70,7 +70,7 @@
              :uberjar {:main aitu.palvelin
                        :aot [aitu.palvelin]}}
   :source-paths ["src/clj" "../../clojure-utils/src/clj"]
-  :java-source-paths ["src/java"]
+  :java-source-paths ["src/java" "../../clojure-utils/src/java"]
   :javac-options ["-target" "1.7" "-source" "1.7"]
   :test-paths ["test/clj"]
   :test-selectors {:kaikki (constantly true)
