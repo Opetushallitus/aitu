7618911167da40b414ca6a2ac9b80fd63c28e6f9
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2015-08-17 08:51:47 +0300

    OPH-1451: Päivitä bower-muutokset

--- ttk/resources/public/vendor/bower-components.js
+++ ttk/resources/public/vendor/bower-components.js
@@ -1363,18 +1363,69 @@ angular.module('ngUpload', [])
 
 'use strict';
 
+angular.module('yhteiset.palvelut.lokalisointi', [] )
+  .filter('lokalisoiKentta', ['kieli', function(kieli){
+    return function(obj, prop) {
+      if (!obj) {
+        return '';
+      }
+      var haluttu = obj[prop + '_' + kieli];
+      if (haluttu) {
+        return haluttu;
+      }
+      var toinenKieli = (kieli === 'fi') ? 'sv' : 'fi';
+      var haluttuToinen = obj[prop + '_' + toinenKieli];
+      if (haluttuToinen) {
+        return haluttuToinen;
+      }
+      return obj[prop];
+    };
+  }])
+  .filter('orderByLokalisoitu', ['$filter', function($filter) {
+    return function(entityt, kentta, reverse){
+      return $filter('orderBy')(entityt, function(entity) {
+        return $filter('lokalisoiKentta')(entity, kentta);
+      }, reverse)
+    };
+  }]);
+
+
+// Copyright (c) 2014 The Finnish National Board of Education - Opetushallitus
+//
+// This program is free software:  Licensed under the EUPL, Version 1.1 or - as
+// soon as they will be approved by the European Commission - subsequent versions
+// of the EUPL (the "Licence");
+//
+// You may not use this work except in compliance with the Licence.
+// You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// European Union Public Licence for more details.
+
+'use strict';
+
 // Aitun services.js:stä.
-angular.module('yhteiset.palvelut.virheLogitus', [])
+angular.module('yhteiset.palvelut.virheLogitus', ['ngCookies'])
 
   .factory('virheLogitusApi', ['$log', '$window', '$injector', function($log, $window, $injector){
+    var virheita = 0;
+
     return {
       lahetaPalvelimelle : function(poikkeus, aiheuttaja){
+        if (virheita >= 10) {
+          return;
+        }
+        virheita++;
 
         var cookies = $injector.get('$cookies'); //Run time injektio circular depencencyn välttämiseksi
 
         try {
           var virheviesti = poikkeus.toString();
           var stackTrace = printStackTrace({ e: poikkeus });
+
+          var ophBaseUrl = (window.ophBaseUrl !== undefined ? window.ophBaseUrl : '');
 
           $.ajax({
             type: 'POST',
