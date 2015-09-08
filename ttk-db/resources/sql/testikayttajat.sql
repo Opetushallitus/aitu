2cd9504a85aba074b5b8b1db0eeea5f5e88e373b
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2015-09-08 11:29:18 +0300

    OPH-1521: Testikäyttäjä Jäsen Järjestö T-9999

--- ttk-db/resources/sql/testikayttajat.sql
+++ ttk-db/resources/sql/testikayttajat.sql
@@ -6,6 +6,9 @@ insert into kayttaja(uid, oid, etunimi, sukunimi, rooli)
 values ('T-1001', 'OID.T-1001', 'Ivan', 'Ivanovits', 'YLLAPITAJA');
 
 insert into kayttaja(uid, oid, etunimi, sukunimi, rooli)
+values ('T-9999', 'OID.T-9999', 'Jäsen', 'Järjestö', 'JARJESTO');
+
+insert into kayttaja(uid, oid, etunimi, sukunimi, rooli)
 values ('osoitepalvelu', 'OID.osoitepalvelu', 'osoitepalvelu', 'api', 'OSOITEPALVELU');
 
 insert into kayttaja(oid, etunimi, sukunimi, rooli, voimassa, uid)
