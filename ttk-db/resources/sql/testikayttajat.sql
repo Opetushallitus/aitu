4fdc256083d2770d4261888bb330dfead5e9e689
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2015-09-15 15:55:33 +0300

    OPH-1534: Lisää testijäsenjärjestö

--- ttk-db/resources/sql/testikayttajat.sql
+++ ttk-db/resources/sql/testikayttajat.sql
@@ -8,6 +8,9 @@ values ('T-1001', 'OID.T-1001', 'Ivan', 'Ivanovits', 'YLLAPITAJA');
 insert into jarjesto(jarjestoid, nimi_fi, keskusjarjestotieto)
 values (-1, 'Testikeskusjärjestö', true);
 
+insert into jarjesto(jarjestoid, nimi_fi, keskusjarjestoid)
+values (-2, 'Testijäsenjärjestö', -1);
+
 insert into kayttaja(uid, oid, etunimi, sukunimi, rooli, jarjesto)
 values ('T-9999', 'OID.T-9999', 'Jäsen', 'Järjestö', 'JARJESTO', -1);
 
