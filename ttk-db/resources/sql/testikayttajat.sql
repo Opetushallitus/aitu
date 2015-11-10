0a3de0a66f4139a99e8964e9823cafced31314c2
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2015-11-10 14:39:09 +0200

    Poistetaan readaituaipal ja osoitepalvelu-käyttäjät testikäyttäjistä

--- ttk-db/resources/sql/testikayttajat.sql
+++ ttk-db/resources/sql/testikayttajat.sql
@@ -14,11 +14,5 @@ values (-2, 'Testijäsenjärjestö', -1);
 insert into kayttaja(uid, oid, etunimi, sukunimi, rooli, jarjesto)
 values ('T-9999', 'OID.T-9999', 'Jäsen', 'Järjestö', 'JARJESTO', -1);
 
-insert into kayttaja(uid, oid, etunimi, sukunimi, rooli)
-values ('osoitepalvelu', 'OID.osoitepalvelu', 'osoitepalvelu', 'api', 'OSOITEPALVELU');
-
 insert into kayttaja(oid, etunimi, sukunimi, rooli, voimassa, uid)
 values('OID.OPH-KATSELIJA', 'Oph', 'Katselija', 'OPH-KATSELIJA', true, 'oph-katselija');
-
-insert into kayttaja(uid, oid, etunimi, sukunimi, rooli)
-values ('readaituaipal', 'OID.readaituaipal', 'readaituaipal', 'api', 'AIPAL');
