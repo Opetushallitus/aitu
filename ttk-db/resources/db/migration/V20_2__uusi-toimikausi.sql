062bf05d07c87656fc764da3377c6f9d205d83cf
Antti Virtanen <antti.virtanen@iki.fi>
2015-11-17 12:24:48 +0200

    korjattu luontikäyttäjä uudelle toimikaudelle. Sotkee muuten toimintaa ja testaamista.

--- ttk-db/resources/db/migration/V20_2__uusi-toimikausi.sql
+++ ttk-db/resources/db/migration/V20_2__uusi-toimikausi.sql
@@ -1,4 +1,6 @@
 -- luodaan uusi toimikausi tietokantaan. Tällä kertaa kaksivuotinen kausi kolmen vuoden sijaan.
 
+set session aitu.kayttaja='JARJESTELMA';
+
 insert into toimikausi (alkupvm, loppupvm, voimassa)
   values (to_date('01082016','DDMMYYYY'), to_date('31072018', 'DDMMYYYY'), false);
