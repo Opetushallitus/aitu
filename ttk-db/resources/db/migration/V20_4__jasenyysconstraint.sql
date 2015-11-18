479d7725b377fe0edfee96f266e435e671ca3402
Antti Virtanen <antti.virtanen@iki.fi>
2015-11-18 10:34:42 +0200

    OPH-1549 jäsenesitysten constraintin lisäksi nimitetty-statuksella alkuperäinen constraint mukaan

--- ttk-db/resources/db/migration/V20_4__jasenyysconstraint.sql
+++ ttk-db/resources/db/migration/V20_4__jasenyysconstraint.sql
@@ -2,4 +2,5 @@ alter table jasenyys drop constraint ei_paallekkaisia_alkupvm;
 alter table jasenyys drop constraint ei_paallekkaisia_loppupvm;
 alter table jasenyys add CONSTRAINT ei_paallekkaisia_alkupvm UNIQUE (henkiloid, toimikunta, alkupvm, esittaja);
 alter table jasenyys add CONSTRAINT ei_paallekkaisia_loppupvm UNIQUE (henkiloid, toimikunta, loppupvm, esittaja);
- 
+
+CREATE UNIQUE INDEX ei_paallekkaisia_nimitysalkupvm ON jasenyys(henkiloid, toimikunta, alkupvm) WHERE status = 'nimitetty';
