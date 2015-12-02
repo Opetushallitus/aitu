6b89619427e12ab0a86222d1639fb2bc110a75fa
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2015-12-02 17:21:51 +0200

    Osoitepalvelu-api uusiksi

--- ttk/src/clj/aitu/rest_api/osoitepalvelu.clj
+++ ttk/src/clj/aitu/rest_api/osoitepalvelu.clj
@@ -1,18 +1,17 @@
 (ns aitu.rest-api.osoitepalvelu
   (:require [aitu.infra.ttk-arkisto :as ttk-arkisto]
             [aitu.infra.oppilaitos-arkisto :as oppilaitos-arkisto]
-            [compojure.api.sweet :refer :all]
             [aitu.toimiala.skeema :refer :all]
-            [aitu.compojure-util :as cu]
+            [aitu.compojure-util :as cu :refer [GET*]]
+            [compojure.api.core :refer [defroutes*]]
             [oph.common.util.http-util :refer [json-response]]))
 
 (defroutes* reitit
   (GET* "/" []
     :summary "Hakee osoitepalvelun tarvitsemat tiedot"
     :return Osoitepalvelu
-    (cu/autorisoitu-transaktio :osoitepalvelu-api nil
-      (let [oppilaitokset (oppilaitos-arkisto/hae-osoitepalvelulle)
-            toimikunnat (ttk-arkisto/hae-osoitepalvelulle)]
-        (json-response {:toimikunnat toimikunnat
-                        :oppilaitokset oppilaitokset}
-                       Osoitepalvelu)))))
+    :kayttooikeus :osoitepalvelu-api
+    (let [oppilaitokset (oppilaitos-arkisto/hae-osoitepalvelulle)
+          toimikunnat (ttk-arkisto/hae-osoitepalvelulle)]
+      (json-response {:toimikunnat toimikunnat
+                      :oppilaitokset oppilaitokset}))))
