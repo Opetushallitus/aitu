f069539d7afc47f53df84d80d322917032283ad0
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2015-12-02 17:34:10 +0200

    Tutkinto-api uusiksi

--- ttk/src/clj/aitu/rest_api/tutkinto.clj
+++ ttk/src/clj/aitu/rest_api/tutkinto.clj
@@ -13,11 +13,11 @@
 ;; European Union Public Licence for more details.
 
 (ns aitu.rest-api.tutkinto
-  (:require [compojure.core :as c]
-            [aitu.infra.tutkinto-arkisto :as arkisto]
+  (:require [aitu.infra.tutkinto-arkisto :as arkisto]
             [aitu.toimiala.tutkinto :as tutkinto]
             [aitu.toimiala.voimassaolo.saanto.tutkinto :as voimassaolo]
-            [aitu.compojure-util :as cu]
+            [aitu.compojure-util :as cu :refer [GET*]]
+            [compojure.api.core :refer [defroutes*]]
             [aitu.util :refer [muodosta-csv]]
             [oph.common.util.http-util :refer [csv-download-response cachable-json-response json-response]]))
 
@@ -27,33 +27,36 @@
                                  :peruste :kieli :ytunnus :koulutustoimija_fi :toimikunta :toimikunta_fi :tilikoodi :toimikausi_alku :toimikausi_loppu
                                  :lukumaara])
 
-(c/defroutes raportti-reitit
-  (c/GET "/csv" req
-    (cu/autorisoitu-transaktio :yleinen-rest-api nil
-      (csv-download-response
-        (muodosta-csv (arkisto/hae-ehdoilla
-                        (assoc (:params req) :avaimet tutkintokenttien-jarjestys))
-                      tutkintokenttien-jarjestys)
-        "tutkinnot.csv")))
-  (c/GET "/raportti" req
-    (cu/autorisoitu-transaktio :raportti nil
-      (csv-download-response
-        (muodosta-csv (arkisto/hae-raportti (assoc (:params req) :avaimet raporttikenttien-jarjestys))
-                       raporttikenttien-jarjestys)
-        "nayttotutkinnot_raportti.csv"))))
+(defroutes* raportti-reitit
+  (GET* "/csv" req
+    :kayttooikeus :yleinen-rest-api
+    (csv-download-response
+      (muodosta-csv (arkisto/hae-ehdoilla
+                      (assoc (:params req) :avaimet tutkintokenttien-jarjestys))
+                    tutkintokenttien-jarjestys)
+      "tutkinnot.csv"))
+  (GET* "/raportti" req
+    :kayttooikeus :raportti
+    (csv-download-response
+      (muodosta-csv (arkisto/hae-raportti (assoc (:params req) :avaimet raporttikenttien-jarjestys))
+                     raporttikenttien-jarjestys)
+      "nayttotutkinnot_raportti.csv")))
 
 (defn rajaa-tutkinnon-kentat [tutkinto]
   (select-keys tutkinto [:tutkintotunnus :nimi_fi :nimi_sv :opintoala_nimi_fi :opintoala_nimi_sv
                          :opintoala :tutkintotaso :peruste :voimassa]))
 
-(c/defroutes reitit
-  (cu/defapi :yleinen-rest-api nil :get "/" [:as req]
+(defroutes* reitit
+  (GET* "/" [:as req]
+    :kayttooikeus :yleinen-rest-api
     (cachable-json-response req (map (comp rajaa-tutkinnon-kentat voimassaolo/taydenna-tutkinnon-voimassaolo)
                                      (arkisto/hae-kaikki))))
-  (cu/defapi :yleinen-rest-api nil :get "/:tutkintotunnus" [tutkintotunnus]
+  (GET* "/:tutkintotunnus" [tutkintotunnus]
+    :kayttooikeus :yleinen-rest-api
     (json-response (tutkinto/taydenna-tutkinto (arkisto/hae tutkintotunnus))))
-  (cu/defapi :yleinen-rest-api nil :get "/haku/osat" [termi :as req]
+  (GET* "/haku/osat" [termi :as req]
+    :kayttooikeus :yleinen-rest-api
     (cachable-json-response req (arkisto/hae-opintoalat-tutkinnot-osaamisalat-tutkinnonosat termi)))
-  (cu/defapi :yleinen-rest-api nil :get "/haku/tutkinnot" [termi :as req]
+  (GET* "/haku/tutkinnot" [termi :as req]
+    :kayttooikeus :yleinen-rest-api
     (cachable-json-response req (arkisto/hae-opintoalat-tutkinnot termi))))
-
