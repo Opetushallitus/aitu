802761a13bf5c25ff139e11ef5b78dd3946acd5d
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2015-12-02 17:27:19 +0200

    Tiedote-api uusiksi

--- ttk/src/clj/aitu/rest_api/tiedote.clj
+++ ttk/src/clj/aitu/rest_api/tiedote.clj
@@ -13,14 +13,14 @@
 ;; European Union Public Licence for more details.
 
 (ns aitu.rest-api.tiedote
-  (:require [compojure.core :as c]
-            [aitu.infra.tiedote-arkisto :as arkisto]
+  (:require [aitu.infra.tiedote-arkisto :as arkisto]
             [oph.common.util.http-util :refer [json-response]]
-            [aitu.compojure-util :as cu]
-            [cheshire.core :as cheshire]))
+            [aitu.compojure-util :as cu :refer [GET* POST* DELETE*]]
+            [compojure.api.core :refer [defroutes*]]))
 
-(c/defroutes reitit
-  (cu/defapi :yleinen-rest-api nil :get "/:tiedoteid" [tiedoteid]
+(defroutes* reitit
+  (GET* "/:tiedoteid" [tiedoteid]
+    :kayttooikeus :yleinen-rest-api
     (let [tiedoteid (Integer/parseInt tiedoteid)
           tiedote (arkisto/hae tiedoteid)]
       (-> (if (nil? tiedote)
@@ -28,11 +28,12 @@
             tiedote)
         json-response)))
 
-  (cu/defapi :tiedote_muokkaus nil :post "/:tiedoteid" [tiedoteid teksti_fi teksti_sv]
+  (POST* "/:tiedoteid" [tiedoteid teksti_fi teksti_sv]
+    :kayttooikeus :tiedote_muokkaus
     (let [tiedote (arkisto/poista-ja-lisaa! (Integer/parseInt tiedoteid) {:teksti_fi teksti_fi :teksti_sv teksti_sv})]
-        {:status 200
-         :body (cheshire/generate-string tiedote)}))
+        (json-response tiedote)))
 
-  (cu/defapi :tiedote_muokkaus nil :delete "/:tiedoteid" [tiedoteid]
+  (DELETE* "/:tiedoteid" [tiedoteid]
+    :kayttooikeus :tiedote_muokkaus
     (arkisto/poista! (Integer/parseInt tiedoteid))
     {:status 200}))
