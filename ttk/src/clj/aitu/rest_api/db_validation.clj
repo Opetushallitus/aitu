dce27a1ba885e2ef3462fabe576522672779a8fd
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2015-12-03 13:03:05 +0200

    Db-validation-api uusiksi

--- ttk/src/clj/aitu/rest_api/db_validation.clj
+++ ttk/src/clj/aitu/rest_api/db_validation.clj
@@ -1,14 +1,14 @@
 (ns aitu.rest-api.db-validation
-  (:require [compojure.core :as c]
-            [aitu.integraatio.sql.validationtest :as validationtest]
-            [oph.common.util.http-util :refer [json-response]]
+  (:require [aitu.integraatio.sql.validationtest :as validationtest]
             [stencil.core :as s]
-            [aitu.compojure-util :as cu]))
+            [aitu.compojure-util :as cu :refer [GET*]]
+            [compojure.api.core :refer [defroutes*]]))
 
 (def query-list @validationtest/default-query-list)
 
-(c/defroutes reitit
-  (cu/defapi :status nil :get "/" []
+(defroutes* reitit
+  (GET* "/" []
+    :kayttooikeus :status
     (let [results-raw (validationtest/run-queries! query-list)
           results (map (fn [rm] {:title (:title rm) :results (map (fn [n] {:result n}) (:results rm))}) results-raw)]
       (s/render-file "html/dbvalidation"
