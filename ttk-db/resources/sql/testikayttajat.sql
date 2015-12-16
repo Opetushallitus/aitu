4e198f24595bca5f86c57ae553d4a24e1b01058d
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2015-12-16 11:53:06 +0200

    OPH-1644: Lisää jäsenjärjestö-käyttäjä testausta varten

--- ttk-db/resources/sql/testikayttajat.sql
+++ ttk-db/resources/sql/testikayttajat.sql
@@ -12,10 +12,14 @@ insert into jarjesto(jarjestoid, nimi_fi, keskusjarjestoid)
 values (-2, 'Testijäsenjärjestö', -1);
 
 insert into kayttaja(uid, oid, etunimi, sukunimi, rooli, jarjesto)
-values ('T-9999', 'OID.T-9999', 'Jäsen', 'Järjestö', 'JARJESTO', -1);
+values
+  ('T-9999', 'OID.T-9999', 'Jäsen', 'Järjestö', 'JARJESTO', -1),
+  ('T-9998', 'OID.T-9998', 'Jäsen', 'Jäsenjärjestö', 'JARJESTO', -2);
 
 insert into henkilo(henkiloid, jarjesto, kayttaja_oid, etunimi, sukunimi, aidinkieli, sukupuoli)
-values (-1000, -1, 'OID.T-9999', 'Jäsen', 'Järjestö', 'fi', 'mies');
+values
+  (-1000, -1, 'OID.T-9999', 'Jäsen', 'Järjestö', 'fi', 'mies'),
+  (-1001, -2, 'OID.T-9998', 'Jäsen', 'Jäsenjärjestö', 'fi', 'mies');
 
 insert into kayttaja(oid, etunimi, sukunimi, rooli, voimassa, uid)
 values('OID.OPH-KATSELIJA', 'Oph', 'Katselija', 'OPH-KATSELIJA', true, 'oph-katselija');
