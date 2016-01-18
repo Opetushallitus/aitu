5fec5fdb605044003e1e9a99146605d6357f20f3
Antti Virtanen <antti.virtanen@iki.fi>
2016-01-18 11:05:56 +0200

    OPH-1677 toimikuntien perustietoja korjattu

--- ttk-db/resources/db/migration/V21_3__toimikuntatietojen_korjaukset.sql
+++ ttk-db/resources/db/migration/V21_3__toimikuntatietojen_korjaukset.sql
@@ -7,7 +7,7 @@ update tutkintotoimikunta set nimi_fi = 'Eläintenhoidon tutkintotoimikunta' whe
 update tutkintotoimikunta set nimi_sv = 'Examenskommissionen inom naturbruk' where diaarinumero = '10/042/2015';
 
 update tutkintotoimikunta set nimi_sv = 'Examenskommissionen för kyltekniska branschen' where diaarinumero = '32/042/2015';
-update tutkintotoimikunta set nimi_sv = 'Examenskommissionen för lantmäteribranschen och teknisk  planering' where diaarinumero = '43/042/2015';
+update tutkintotoimikunta set nimi_sv = 'Examenskommissionen för lantmäteribranschen och teknisk planering' where diaarinumero = '43/042/2015';
 
 update tutkintotoimikunta set nimi_fi = 'Sosiaali- ja terveysalan tutkintotoimikunta' where diaarinumero = '70/042/2015';
 update tutkintotoimikunta set nimi_sv = 'Yrittäjyysalan tutkintotoimikunta' where diaarinumero = '93/042/2015';
