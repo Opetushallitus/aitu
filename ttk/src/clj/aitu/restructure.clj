092b72111cfb6398606656e57b51f4d99675ef55
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2016-02-17 11:05:18 +0200

    OPH-1705: restructure-param luettavammaksi

--- ttk/src/clj/aitu/restructure.clj
+++ ttk/src/clj/aitu/restructure.clj
@@ -4,14 +4,13 @@
             [aitu.compojure-util :as cu]))
 
 (defmethod compojure.api.meta/restructure-param :kayttooikeus
-  [_ kayttooikeus {:keys [body] :as acc}]
+  [_ kayttooikeus_spec {:keys [body] :as acc}]
   "Käyttoikeuslaajennos compojure-apin rajapintoihin. Esim:
 
   :kayttooikeus :jasenesitys-poisto
   :kayttooikeus [:jasenesitys-poisto jasenyysid]"
 
-  (let [konteksti (when (vector? kayttooikeus) (second kayttooikeus))
-        kayttooikeus (if (vector? kayttooikeus) (first kayttooikeus) kayttooikeus)]
+  (let [[kayttooikeus konteksti] (if (vector? kayttooikeus_spec) kayttooikeus_spec [kayttooikeus_spec])]
     (-> acc
         (assoc-in [:swagger :description] (str "Käyttöoikeus " kayttooikeus " , konteksti: " (or konteksti "N/A")))
         (assoc :body [`(cu/autorisoitu-transaktio ~kayttooikeus ~konteksti (do ~@body))]))))
