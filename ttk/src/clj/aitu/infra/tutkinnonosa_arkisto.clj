ef34cd6ff9c23405d6b4d688e52eb57f79ed3d32
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2016-05-30 10:44:47 +0300

    OPH-1769 Käyttöliittymä uuden tietomallin mukaiseksi

--- ttk/src/clj/aitu/infra/tutkinnonosa_arkisto.clj
+++ ttk/src/clj/aitu/infra/tutkinnonosa_arkisto.clj
@@ -29,8 +29,7 @@
   [tutkintotunnus]
   (->
     (sql/select* :tutkinnonosa)
-    (sql/join :tutkinto_ja_tutkinnonosa (= :tutkinto_ja_tutkinnonosa.tutkinnonosa :tutkinnonosa_id))
-    (sql/join :tutkintoversio (= :tutkintoversio.tutkintoversio_id :tutkinto_ja_tutkinnonosa.tutkintoversio))
+    (sql/join :tutkintoversio (= :tutkintoversio.tutkintoversio_id :tutkinnonosa.tutkintoversio))
     (sql/join :nayttotutkinto (= :nayttotutkinto.tutkintotunnus :tutkintoversio.tutkintotunnus))
     (sql/fields :osatunnus :nimi_fi :nimi_sv :tutkinnonosa_id
                 [:nayttotutkinto.nimi_fi :nayttotutkinto_nimi_fi]
