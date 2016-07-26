0a2fbacfa80dad1169c6d7161866586833183649
Antti Virtanen <antti.virtanen@solita.fi>
2016-07-26 12:31:17 +0300

    Kaksi testiopiskelijaa suoritustietojen testaamista varten

--- ttk-db/resources/sql/testidata.sql
+++ ttk-db/resources/sql/testidata.sql
@@ -61,3 +61,10 @@ insert into jarjestamissopimus (jarjestamissopimusid, sopimusnumero, toimikunta,
 
 insert into sopimus_ja_tutkinto(jarjestamissopimusid, tutkintoversio)
   values (-324601, -10000);
+
+insert into suorittaja (suorittaja_id, etunimi, sukunimi, oid)
+  values (-1, 'Orvokki', 'Opiskelija', 'fan.far.12345');
+
+insert into suorittaja (suorittaja_id, etunimi, sukunimi, oid)
+  values (-2, 'Lieto', 'Lemminkäinen', 'pfft.12345');
+
