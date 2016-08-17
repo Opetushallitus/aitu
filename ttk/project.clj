5257a9b9cbff803d9c4222ee1e494aad38655c3d
Antti Virtanen <antti.virtanen@solita.fi>
2016-08-17 11:03:47 +0300

    OPH-1535 http-kit versio päivitetty. Korjaa logitusongelman.

--- ttk/project.clj
+++ ttk/project.clj
@@ -24,7 +24,7 @@
                  [com.cemerick/valip "0.3.2"]
                  [com.jolbox/bonecp "0.8.0.RELEASE"]
                  [compojure "1.4.0"]
-                 [http-kit "2.1.18"]
+                 [http-kit "2.1.19"]
                  [javax.servlet/servlet-api "2.5"]
                  [korma "0.3.0-RC6"]
                  [metosin/compojure-api "1.0.0-RC2"]
