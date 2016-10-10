fda38520ec2d39ff0cdcc7eef25fe11bc6b9421e
Antti Virtanen <antti.virtanen@solita.fi>
2016-10-10 09:41:28 +0300

    Korjattu migraatioskripti siten että olemassaolevat arvioijat eivät aiheuta ongelmia tietokannan migraatiossa.

--- ttk-db/resources/db/migration/V34_1__suoritustiedot.sql
+++ ttk-db/resources/db/migration/V34_1__suoritustiedot.sql
@@ -1,7 +1,14 @@
 alter table arvioija
+  add column etunimi varchar(40),
+  add column sukunimi varchar(40);
+  
+update arvioija set etunimi = '-';
+update arvioija set sukunimi = nimi;
+  
+alter table arvioija
   drop column nimi,
-  add column etunimi varchar(40) not null,
-  add column sukunimi varchar(40) not null;
+  alter column etunimi set not null,
+  alter column sukunimi set not null; 
   
 alter table suoritus
   drop column osaamisen_tunnustaminen,
