b7c8e1bd718a6f7e71631e41e33bd9dbb7d6c3c7
Antti Virtanen <antti.virtanen@solita.fi>
2016-10-13 15:23:21 +0300

    OPH-1862 korjattu tietokantarajoitetta suorittaja-taulusta siten että useampi null oid/hetu on mahdollinen.

--- ttk-db/resources/db/migration/V35_1__suoritustiedot.sql
+++ ttk-db/resources/db/migration/V35_1__suoritustiedot.sql
@@ -2,3 +2,8 @@ alter table suorituskerta
   alter column jarjestelyt type varchar(1000);
 
 comment on column suorituskerta.jarjestelyt is 'Vapaa kuvaus työtehtävistä ja järjestelyistä, jotka liittyvät näyttötilaisuuteen.';
+
+alter table suorittaja
+  drop constraint suorittaja_oid_key,
+  drop constraint suorittaja_hetu_key,
+  add CONSTRAINT oidhetu_constraint UNIQUE (hetu, oid);
