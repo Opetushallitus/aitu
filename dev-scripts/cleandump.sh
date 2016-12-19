d7feece08643aaaf0df36a3d9a43d5ce3ed336a7
Antti Virtanen <antti.virtanen@solita.fi>
2016-12-19 16:01:10 +0200

    OPH-1912 dumpissa olevan henkilötiedon tyhjentävä skripti

---
+++ dev-scripts/cleandump.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eu
+
+REPO_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+
+set -x
+
+cd $REPO_PATH/vagrant
+vagrant ssh db -c 'cd /env && ./pgload-ttk.sh /dumps/amtu-dump.db dev-db.pgpass ttk ttk_adm 127.0.0.1'
+
+cd $REPO_PATH/ttk-db
+lein run 'postgresql://ttk_adm:ttk-adm@127.0.0.1:2345/ttk' -u ttk_user --aituhaku-username aituhaku_user
+
+cd $REPO_PATH/vagrant
+vagrant ssh db -c 'cd /env && ./pgdump-ttk.sh /dumps/amtu-dump.db dev-db.pgpass ttk ttk_adm 127.0.0.1'
