92a23d1f4955b060df0a115a85e40e5743a1612e
Jari Asp <jari.asp@solita.fi>
2017-05-19 17:52:29 +0300

    OPH-1953 "Suoritushaku ei aina osaa päivittää ruudulla näkyvää tulosjoukkoa autocomplete-haussa" -> Lisätty 0,5 sekunnin debounce ennen haeSuoritukset-requestin lähettämistä, ja edellinen requesti canceloidaan (angularin $timeout:lla), jos se on vielä kesken.

--- ttk/resources/public/js/direktiivit/suoritukset.js
+++ ttk/resources/public/js/direktiivit/suoritukset.js
@@ -20,10 +20,14 @@ angular.module('direktiivit.suoritukset', ['rest.suoritus'])
       restrict: 'E',
       templateUrl: 'template/direktiivit/suoritukset',
       scope: {},
-      controller: ['$location', '$scope', 'Toimikunta', 'Koulutustoimija', 'Suoritus', 'TutkintoResource', 'Tutkinnonosa', 'Osaamisala', 'Varmistus', 'i18n',
-         function($location, $scope, Toimikunta, Koulutustoimija, Suoritus, TutkintoResource, Tutkinnonosa, Osaamisala, Varmistus, i18n) {
-        $scope.hakuForm = {tila: null,
-                   tuloksiasivulla: "10"
+      controller: ['$location', '$scope', '$timeout', 'Toimikunta', 'Koulutustoimija', 'Suoritus', 'TutkintoResource', 'Tutkinnonosa', 'Osaamisala', 'Varmistus', 'i18n',
+         function($location, $scope, $timeout, Toimikunta, Koulutustoimija, Suoritus, TutkintoResource, Tutkinnonosa, Osaamisala, Varmistus, i18n) {
+
+        var haeSuorituksetTimeoutPromise = null;
+
+        $scope.hakuForm = {
+            tila: null,
+            tuloksiasivulla: "10"
         };
         $scope.filterTila = function(tila) {
           return function(item) {
@@ -79,13 +83,14 @@ angular.module('direktiivit.suoritukset', ['rest.suoritus'])
         });
 
         $scope.poistaSuoritus = function(poistettavaSuoritus) {
-          Varmistus.varmista(i18n.arviointipaatokset.poistetaanko_suoritus, i18n.arviointipaatokset.poista_suoritus_teksti, i18n.arviointipaatokset.poista_suoritus).then(function() {
-            Suoritus.poista(poistettavaSuoritus.suorituskerta_id).then(function() {
-              _.remove($scope.suoritukset, function(suoritus) {
-                return suoritus.suorituskerta_id == poistettavaSuoritus.suorituskerta_id;
-              })
+          Varmistus.varmista(i18n.arviointipaatokset.poistetaanko_suoritus, i18n.arviointipaatokset.poista_suoritus_teksti, i18n.arviointipaatokset.poista_suoritus)
+            .then(function() {
+              Suoritus.poista(poistettavaSuoritus.suorituskerta_id).then(function() {
+                _.remove($scope.suoritukset, function(suoritus) {
+                  return suoritus.suorituskerta_id == poistettavaSuoritus.suorituskerta_id;
+                })
+              });
             });
-          });
         };
 
         $scope.muokkaaSuoritusta = function(muokattavaSuoritus) {
@@ -114,49 +119,58 @@ angular.module('direktiivit.suoritukset', ['rest.suoritus'])
         };
 
         $scope.palautaLuonnokseksi = function() {
-          Varmistus.varmista(i18n.arviointipaatokset.palautetaanko_suoritus_luonnokseksi, i18n.arviointipaatokset.palauta_suoritus_luonnokseksi_teksti, i18n.arviointipaatokset.palauta_suoritus_luonnokseksi).then(function() {
-            var valitutSuoritukset = $scope.valitutSuoritukset();
-            Suoritus.palauta(valitutSuoritukset).then(function() {
-              paivitaSuoritustenTila(valitutSuoritukset, 'luonnos');
+          Varmistus.varmista(i18n.arviointipaatokset.palautetaanko_suoritus_luonnokseksi, i18n.arviointipaatokset.palauta_suoritus_luonnokseksi_teksti, i18n.arviointipaatokset.palauta_suoritus_luonnokseksi)
+            .then(function() {
+              var valitutSuoritukset = $scope.valitutSuoritukset();
+              Suoritus.palauta(valitutSuoritukset).then(function() {
+                paivitaSuoritustenTila(valitutSuoritukset, 'luonnos');
+              });
             });
-          });
         };
 
         $scope.palautaHyvaksyttavaksi = function() {
-          Varmistus.varmista(i18n.arviointipaatokset.palautetaanko_suoritus_hyvaksyttavaksi, i18n.arviointipaatokset.palauta_suoritus_hyvaksyttavaksi_teksti, i18n.arviointipaatokset.palauta_suoritus_hyvaksyttavaksi).then(function() {
-            var valitutSuoritukset = $scope.valitutSuoritukset();
-            Suoritus.lahetaHyvaksyttavaksi(valitutSuoritukset).then(function() {
-              paivitaSuoritustenTila(valitutSuoritukset, 'ehdotettu');
+          Varmistus.varmista(i18n.arviointipaatokset.palautetaanko_suoritus_hyvaksyttavaksi, i18n.arviointipaatokset.palauta_suoritus_hyvaksyttavaksi_teksti, i18n.arviointipaatokset.palauta_suoritus_hyvaksyttavaksi)
+            .then(function() {
+              var valitutSuoritukset = $scope.valitutSuoritukset();
+              Suoritus.lahetaHyvaksyttavaksi(valitutSuoritukset).then(function() {
+                paivitaSuoritustenTila(valitutSuoritukset, 'ehdotettu');
+              });
             });
-          });
         };
 
 
         $scope.lahetaHyvaksyttavaksi = function() {
-          Varmistus.varmista(i18n.arviointipaatokset.esitetaanko_suoritus, i18n.arviointipaatokset.esita_suoritus_teksti, i18n.arviointipaatokset.esita_suoritus).then(function() {
-            var valitutSuoritukset = $scope.valitutSuoritukset();
-            Suoritus.lahetaHyvaksyttavaksi(valitutSuoritukset).then(function() {
-              paivitaSuoritustenTila(valitutSuoritukset, 'ehdotettu');
+          Varmistus.varmista(i18n.arviointipaatokset.esitetaanko_suoritus, i18n.arviointipaatokset.esita_suoritus_teksti, i18n.arviointipaatokset.esita_suoritus)
+            .then(function() {
+              var valitutSuoritukset = $scope.valitutSuoritukset();
+              Suoritus.lahetaHyvaksyttavaksi(valitutSuoritukset).then(function() {
+                paivitaSuoritustenTila(valitutSuoritukset, 'ehdotettu');
+              });
             });
-          });
         };
 
         $scope.hyvaksy = function() {
-          Varmistus.varmista(i18n.arviointipaatokset.hyvaksytaanko_suoritus, i18n.arviointipaatokset.hyvaksy_suoritus_teksti, i18n.arviointipaatokset.hyvaksy_suoritus).then(function() {
-            var valitutSuoritukset = $scope.valitutSuoritukset();
-            Suoritus.hyvaksy(valitutSuoritukset, $scope.form.hyvaksymispvm).then(function() {
-              paivitaSuoritustenTila(valitutSuoritukset, 'hyvaksytty');
+          Varmistus.varmista(i18n.arviointipaatokset.hyvaksytaanko_suoritus, i18n.arviointipaatokset.hyvaksy_suoritus_teksti, i18n.arviointipaatokset.hyvaksy_suoritus)
+            .then(function() {
+              var valitutSuoritukset = $scope.valitutSuoritukset();
+              Suoritus.hyvaksy(valitutSuoritukset, $scope.form.hyvaksymispvm).then(function() {
+                paivitaSuoritustenTila(valitutSuoritukset, 'hyvaksytty');
+              });
             });
-          });
         };
 
         $scope.haeSuoritukset = function() {
-          Suoritus.haeKaikki($scope.hakuForm).then(function(suoritukset) {
-          $scope.suoritukset = suoritukset;
-          $scope.suorituksetjarjestetty = suoritukset;
-          $scope.suoritussivu = 1;
+
+          if (haeSuorituksetTimeoutPromise) {
+            $timeout.cancel(haeSuorituksetTimeoutPromise);  // cancel the previous pending request
+          }
+          haeSuorituksetTimeoutPromise = $timeout(Suoritus.haeKaikki, 500, true, $scope.hakuForm);  // 500ms = 0,5s
+          haeSuorituksetTimeoutPromise.then(function(suoritukset) {
+            $scope.suoritukset = suoritukset;
+            $scope.suorituksetjarjestetty = suoritukset;
+            $scope.suoritussivu = 1;
           })
-          };
+        };
 
         $scope.$watch('hakuForm', function(hakuForm, oldhakuform) {
           if (! (angular.equals(hakuForm,oldhakuform))) { // ei haeta ensimmäisellä sivulatauksella dataa
