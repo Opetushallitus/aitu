b1bfbe01d826ee77564728d8e9496bc98d54c60c
Jari Asp <jari.asp@solita.fi>
2017-06-01 10:18:31 +0300

    refactoring: removed duplication

--- ttk/src/clj/aitu/infra/sopimus_ja_tutkinto_arkisto.clj
+++ ttk/src/clj/aitu/infra/sopimus_ja_tutkinto_arkisto.clj
@@ -20,44 +20,47 @@
             [aitu.integraatio.sql.tutkinto :as tutkinto-kaytava]
             [aitu.integraatio.sql.korma :refer :all]))
 
-(defn liita-sopimus-ja-tutkinto-riviin
-  [tutkintoversion-haku-fn sopimus-ja-tutkinto]
-  (let [{:keys [jarjestamissopimusid sopimus_ja_tutkinto_id tutkintoversio]} sopimus-ja-tutkinto]
-    (-> sopimus-ja-tutkinto
-      (assoc :tutkintoversio (tutkintoversion-haku-fn tutkintoversio))
-      (assoc :jarjestamissopimus (sopimus-kaytava/hae jarjestamissopimusid))
-      (assoc :jarjestamissuunnitelmat
-             (sopimus-kaytava/hae-sopimus-ja-tutkinto-rivin-jarjestamissuunnitelmat sopimus_ja_tutkinto_id))
-      (assoc :liitteet (sopimus-kaytava/hae-sopimus-ja-tutkinto-rivin-liitteet sopimus_ja_tutkinto_id)))))
 
-(defn liita-sopimus-ja-tutkinto-riveihin
-  [tutkintoversion-haku-fn rivit]
-  (mapv (partial liita-sopimus-ja-tutkinto-riviin tutkintoversion-haku-fn) rivit))
+(def ^:private valittavat-kentat-perus [:tutkintoversio.peruste :nayttotutkinto.tutkintotunnus
+                                        :nayttotutkinto.nimi_fi :nayttotutkinto.nimi_sv])
 
-(defn hae-jarjestamissopimuksen-tutkinnot-rajatut-tiedot
-  [jarjestamissopimusid]
+(def ^:private valittavat-kentat-laaja (concat valittavat-kentat-perus [:tutkintoversio.voimassa_alkupvm
+                                                                        :tutkintoversio.voimassa_loppupvm
+                                                                        :tutkintoversio.siirtymaajan_loppupvm
+                                                                        :sopimus_ja_tutkinto.sopimus_ja_tutkinto_id]))
+
+(defn ^:private do-hae-jarjestamissopimuksen-tutkinnot
+  [jarjestamissopimusid valittavat-kentat]
   (sql/select sopimus-ja-tutkinto
     (sql/with tutkintoversio
       (sql/with nayttotutkinto))
     (sql/where {:jarjestamissopimusid jarjestamissopimusid
                 :poistettu false})
-    (sql/fields :tutkintoversio.peruste :nayttotutkinto.tutkintotunnus
-                :nayttotutkinto.nimi_fi :nayttotutkinto.nimi_sv)))
+    (apply sql/fields valittavat-kentat)))
+
+(defn hae-jarjestamissopimuksen-tutkinnot-rajatut-tiedot
+  [jarjestamissopimusid]
+  (do-hae-jarjestamissopimuksen-tutkinnot jarjestamissopimusid valittavat-kentat-perus))
 
 (defn hae-jarjestamissopimuksen-tutkinnot
   [jarjestamissopimusid]
-  (let [sopimus-ja-tutkinto-rivit (sql/select sopimus-ja-tutkinto
-                                    (sql/with tutkintoversio
-                                      (sql/with nayttotutkinto))
-                                    (sql/where {:jarjestamissopimusid jarjestamissopimusid
-                                                :poistettu false})
-                                    (sql/fields :tutkintoversio.peruste :nayttotutkinto.nimi_fi :nayttotutkinto.nimi_sv
-                                                :tutkintoversio.voimassa_alkupvm :tutkintoversio.voimassa_loppupvm
-                                                :tutkintoversio.siirtymaajan_loppupvm :nayttotutkinto.tutkintotunnus
-                                                :sopimus_ja_tutkinto.sopimus_ja_tutkinto_id))]
+  (let [sopimus-ja-tutkinto-rivit (do-hae-jarjestamissopimuksen-tutkinnot jarjestamissopimusid valittavat-kentat-laaja)]
     (for [rivi sopimus-ja-tutkinto-rivit]
       {:sopimus_ja_tutkinto_id (:sopimus_ja_tutkinto_id rivi)
-       :tutkintoversio (dissoc rivi :sopimus_ja_tutkinto_id)})))
+       :tutkintoversio         (dissoc rivi :sopimus_ja_tutkinto_id)})))
+
+(defn liita-sopimus-ja-tutkinto-riviin
+  [tutkintoversion-haku-fn sopimus-ja-tutkinto]
+  (let [{:keys [jarjestamissopimusid sopimus_ja_tutkinto_id tutkintoversio]} sopimus-ja-tutkinto]
+    (merge sopimus-ja-tutkinto
+      {:tutkintoversio          (tutkintoversion-haku-fn tutkintoversio)
+       :jarjestamissopimus      (sopimus-kaytava/hae jarjestamissopimusid)
+       :jarjestamissuunnitelmat (sopimus-kaytava/hae-sopimus-ja-tutkinto-rivin-jarjestamissuunnitelmat sopimus_ja_tutkinto_id)
+       :liitteet                (sopimus-kaytava/hae-sopimus-ja-tutkinto-rivin-liitteet sopimus_ja_tutkinto_id)})))
+
+(defn liita-sopimus-ja-tutkinto-riveihin
+  [tutkintoversion-haku-fn rivit]
+  (mapv (partial liita-sopimus-ja-tutkinto-riviin tutkintoversion-haku-fn) rivit))
 
 (defn hae-jarjestamissopimukseen-liittyvat
   "Hakee järjestämissopimukseen liittyvät sopimus-ja-tutkinto-tiedot"
